c849f30614822be6d05ff46f95a683e0ac923670
mraad <mraad@esri.com>
2020-02-11 05:43:47 -0500

    Added to/from hex nume

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ organization := "com.esri"
 
 name := "grid-hex"
 
-version := "2.0"
+version := "2.1"
 
 isSnapshot := true
 
--- src/main/scala/com/esri/hex/Layout.scala
+++ src/main/scala/com/esri/hex/Layout.scala
@@ -41,14 +41,7 @@ case class Layout(origX: Double, origY: Double, sizeX: Double, sizeY: Double, or
     FractionalHex(q, r, -q - r)
   }
 
-  /**
-    * Convert world coordinate to a QR hex key.
-    *
-    * @param x the world x coordinate.
-    * @param y the world y coordinate.
-    * @return a key as a String instance.
-    */
-  def xyToKey(x: Double, y: Double): String = {
+  private def xy2qr(x: Double, y: Double): (Int, Int) = {
     val px = (x - origX) / sizeX
     val py = (y - origY) / sizeY
     val q = orientation.b0 * px + orientation.b1 * py
@@ -65,7 +58,40 @@ case class Layout(origX: Double, origY: Double, sizeX: Double, sizeY: Double, or
 
     if (qAbs > rAbs && qAbs > sAbs) qInt = -rInt - sInt
     else if (rAbs > sAbs) rInt = -qInt - sInt
+    (qInt, rInt)
+  }
+
+  /**
+    * Convert world coordinate to a QR hex key.
+    *
+    * @param x the world x coordinate.
+    * @param y the world y coordinate.
+    * @return a key as a String instance.
+    */
+  def xyToKey(x: Double, y: Double): String = {
+    //    val px = (x - origX) / sizeX
+    //    val py = (y - origY) / sizeY
+    //    val q = orientation.b0 * px + orientation.b1 * py
+    //    val r = orientation.b2 * px + orientation.b3 * py
+    //    val s = -q - r
+    //
+    //    var qInt = q.round.toInt
+    //    var rInt = r.round.toInt
+    //    val sInt = s.round.toInt
+    //
+    //    val qAbs = abs(qInt - q)
+    //    val rAbs = abs(rInt - r)
+    //    val sAbs = abs(sInt - s)
+    //
+    //    if (qAbs > rAbs && qAbs > sAbs) qInt = -rInt - sInt
+    //    else if (rAbs > sAbs) rInt = -qInt - sInt
+    val (qInt, rInt) = xy2qr(x, y)
     s"$qInt:$rInt"
+  }
+
+  def xyToNume(x: Double, y: Double): Long = {
+    val (qInt, rInt) = xy2qr(x, y)
+    (qInt << 32) | (rInt & 0xFFFFFFFF)
   }
 
   /**
